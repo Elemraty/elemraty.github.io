0f7e60dfb389923921d86bba0c7d3c3e5bc36489
Elemraty <loveaaak12@gmail.com>
2025-02-17 14:58:18 +0900

    env

--- stockmanager/src/firebase.js
+++ stockmanager/src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
-  apiKey: "AIzaSyAh0Zhxt_sAbAtMeW_gb0-QOaT1u7KHZH8",
-  authDomain: "visualp-1.firebaseapp.com",
-  databaseURL: "https://visualp-1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "visualp-1",
-  storageBucket: "visualp-1.firebasestorage.app",
-  messagingSenderId: "659233295218",
-  appId: "1:659233295218:web:bc8889bbdcd00a4f372beb",
-  measurementId: "G-T34PEL0VS4"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
